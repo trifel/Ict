8d3bd33d276dfff11611415b342ce6adcacee733
Come-from-Beyond <come-from-beyond@mail.ru>
2018-10-18 20:32:03 +0300

    Properties initialization.

--- src/cfb/ict/Ict.java
+++ src/cfb/ict/Ict.java
@@ -1,4 +1,24 @@
 package cfb.ict;
 
 public class Ict {
+
+    static final String VERSION = "0.2.0";
+
+    static Properties properties;
+
+    public static void main(final String[] args) {
+
+        Utils.log("Ict " + VERSION + " is launched.");
+
+        try {
+
+            properties = new Properties(args[0]);
+
+        } catch (final RuntimeException e) {
+
+            properties = new Properties();
+        }
+
+        Utils.log("Ict " + VERSION + " is shut down.");
+    }
 }
--- src/cfb/ict/Properties.java
+++ src/cfb/ict/Properties.java
@@ -4,12 +4,15 @@ import java.io.FileInputStream;
 
 public class Properties {
 
-    final String host;
-    final int port;
-    final int roundDuration; // In milliseconds
-    final int minEchoDelay, maxEchoDelay; // In milliseconds
+    String host = "localhost";
+    int port = 14265;
+    int roundDuration = 60000; // In milliseconds
+    int minEchoDelay = 0, maxEchoDelay = 5000; // In milliseconds
 
-    final long timestampLowerBoundDelta, timestampUpperBoundDelta; // In seconds
+    long timestampLowerBoundDelta = 90, timestampUpperBoundDelta = 90; // In seconds
+
+    Properties() {
+    }
 
     Properties(final String fileName) {
 
@@ -24,13 +27,13 @@ public class Properties {
             throw new RuntimeException(e);
         }
 
-        host = properties.getProperty("host", "localhost");
-        port = Integer.parseInt(properties.getProperty("port", "14265"));
-        roundDuration = Integer.parseInt(properties.getProperty("roundDuration", "60000"));
-        minEchoDelay = Integer.parseInt(properties.getProperty("minEchoDelay", "0"));
-        maxEchoDelay = Integer.parseInt(properties.getProperty("maxEchoDelay", "5000"));
+        host = properties.getProperty("host", host);
+        port = Integer.parseInt(properties.getProperty("port", Integer.valueOf(port).toString()));
+        roundDuration = Integer.parseInt(properties.getProperty("roundDuration", Integer.valueOf(roundDuration).toString()));
+        minEchoDelay = Integer.parseInt(properties.getProperty("minEchoDelay", Integer.valueOf(minEchoDelay).toString()));
+        maxEchoDelay = Integer.parseInt(properties.getProperty("maxEchoDelay", Integer.valueOf(maxEchoDelay).toString()));
 
-        timestampLowerBoundDelta = Long.parseLong(properties.getProperty("timestampLowerBoundDelta", "90"));
-        timestampUpperBoundDelta = Long.parseLong(properties.getProperty("timestampUpperBoundDelta", "90"));
+        timestampLowerBoundDelta = Long.parseLong(properties.getProperty("timestampLowerBoundDelta", Long.valueOf(timestampLowerBoundDelta).toString()));
+        timestampUpperBoundDelta = Long.parseLong(properties.getProperty("timestampUpperBoundDelta", Long.valueOf(timestampUpperBoundDelta).toString()));
     }
 }
