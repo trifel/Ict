60862ecdfe3c925af5eb0991d6a751a9513be66c
Come-from-Beyond <come-from-beyond@mail.ru>
2018-10-28 11:54:50 +0300

    Hash.

---
+++ src/cfb/ict/Hash.java
@@ -0,0 +1,37 @@
+package cfb.ict;
+
+import java.util.Arrays;
+
+public class Hash {
+
+    static final int LENGTH = 243;
+
+    final byte[] trits;
+
+    private int hashCode;
+
+    Hash(final byte[] trits, final int offset) {
+
+        this.trits = Arrays.copyOfRange(trits, offset, offset + LENGTH);
+
+        hashCode = Arrays.hashCode(this.trits);
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+
+        return Arrays.equals(trits, ((Hash) obj).trits); // The class check is omitted on purpose
+    }
+
+    @Override
+    public int hashCode() {
+
+        return hashCode;
+    }
+
+    @Override
+    public String toString() {
+
+        return Utils.trytes(trits, 0, trits.length);
+    }
+}
